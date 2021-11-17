ecf96ad9114161655718eb2f6c46d453808e564e
Touhidul Shawan <touhidulshawan@gmail.com>
2021-11-18 05:36:34 +0600

    onFocus get postID on Discussion

--- components/post/Discussion.jsx
+++ components/post/Discussion.jsx
@@ -1,6 +1,31 @@
-const Discussion = () => {
+import { useEffect, useState } from "react";
+import { db } from "../../utils/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { useRouter } from "next/router";
+import { useAuth } from "../../context/AuthContext";
+
+const Discussion = ({ getID, postID }) => {
+  const [commentText, setCommentText] = useState("");
+  const router = useRouter();
+  const { currentUser } = useAuth();
+  const { uid, displayName } = currentUser;
+  console.log(postID);
+  useEffect(async () => {
+    const docRef = doc(db, "posts", uid);
+    const docSnap = await getDoc(docRef);
+    const data = docSnap.data();
+  });
+
+  // submit comment
+  const submitComment = async (evt) => {
+    evt.preventDefault();
+    // logic goes here
+    setCommentText("");
+    router.reload();
+  };
+
   return (
-    <form>
+    <form onSubmit={submitComment}>
       <textarea
         name="discussionBox"
         id="discussionBox"
@@ -9,6 +34,9 @@ const Discussion = () => {
         maxLength="2000"
         placeholder="Write your opinion here....."
         className="w-full p-4 h-36 bg-bluegray-600 border-2 border-bluegray-600 rounded-lg resize-none focus:outline-sky tracking-wider leading-7 break-words"
+        value={commentText}
+        onChange={(evt) => setCommentText(evt.target.value)}
+        onFocus={() => getID()}
       ></textarea>
       <button
         className="bg-purple-800 text-purple-200 py-3 px-6 mt-3 border-b-8 border-fuchsia-500 rounded-lg uppercase text-sm tracking-wider font-righteous text-center hover:border-yellow-400 transition-all duration-500 focus:outline-sky"
--- components/post/Post.jsx
+++ components/post/Post.jsx
@@ -1,19 +1,27 @@
 import PostCard from "./PostCard";
 import Discussion from "./Discussion";
+import { useState } from "react";
 
 const Post = (props) => {
-  return (
-    <div className=" bg-bluegray-800  w-6/12 shadow-xl mb-2 rounded">
-      <PostCard {...props} />
+  const [getCurrentID, setCurrentID] = useState(null);
+  const getID = (id) => {
+    setCurrentID(id);
+  };
+  return props.posts.map((post) => (
+    <div
+      key={post.id}
+      className=" bg-bluegray-800  w-6/12 shadow-xl mb-2 rounded"
+    >
+      <PostCard {...post} />
       {/* discussion panel */}
       <div className=" bg-bluegray-900">
         <div className="p-5">
           <h4 className="mb-2 text-bluegray-500 capitalize">
             Give your opinion
           </h4>
-          <Discussion />
+          <Discussion getID={() => getID(post.id)} postID={getCurrentID} />
         </div>
-        {props.comments && props.comments.length !== 0 ? (
+        {post.comments && post.comments.length !== 0 ? (
           <PostCard />
         ) : (
           <p className="py-3 text-center block bg-bluegray-600 text-bluegray-100 tracking-widest">
@@ -22,6 +30,6 @@ const Post = (props) => {
         )}
       </div>
     </div>
-  );
+  ));
 };
 export default Post;
